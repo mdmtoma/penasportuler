4c818dff91ca06631cebb14adc391de0997c9cce
Marius Toma <marius.toma@made.com>
2020-01-20 22:41:48 +0200

    removed document ready comments

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,4 @@
-
-//$(document).ready(function() {
-    $('.show-form').on('click', function() {
-        $('form').show();
-        $('.show-form').hide();
-    });
-//});
+$('.show-form').on('click', function() {
+    $('form').show();
+    $('.show-form').hide();
+});
